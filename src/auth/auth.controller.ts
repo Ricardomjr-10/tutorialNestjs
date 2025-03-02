f0401f08acc7f06f0126cdac41164aebfde8ac03
Ricardo-10 <ricardomjr2@gmail.com>
2025-03-02 16:59:13 -0300

    add rota post auth

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
+import { AuthResponseDto } from './auth.dto';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('login')
+  signIn(
+    @Body('username') username: string,
+    @Body('password') password: string,
+  ): AuthResponseDto {
+    return this.authService.signIn(username, password);
+  }
+}
