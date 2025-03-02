c971f1ef0b0b0fcc889a4df1b9cfb2f59d667c4c
Ricardo-10 <ricardomjr2@gmail.com>
2025-03-02 17:19:06 -0300

    atualizando auth module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
+  imports: [
+    JwtModule.registerAsync({
+      global: true,
+      imports: [],
+      useFactory: async (confingService: ConfigService) => ({
+        secret: confingService.get<string>('JWT_SECRET'),
+        signOptions: {
+          expiresIn: +confingService.get<number>('JWT_EXPIRATION_TIME'), // O + É PRA CONVERTER PRA NUMBER
+        },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   providers: [AuthService],
-  controllers: [AuthController]
+  controllers: [AuthController],
 })
 export class AuthModule {}
