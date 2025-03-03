d0c9706c6e15565fdadd6f1c2c8a997ccf29f7f2
Ricardo-10 <ricardomjr2@gmail.com>
2025-03-03 17:37:17 -0300

    erros corrigidos

--- src/auth/auth.guard.ts
+++ src/auth/auth.guard.ts
@@ -4,12 +4,13 @@ import {
   Injectable,
   UnauthorizedException,
 } from '@nestjs/common';
+import { Request } from 'express';
 import { ConfigService } from '@nestjs/config';
 import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-  private jwtSecret: string;
+  private jwtSecret: string | undefined;
 
   constructor(
     private readonly jwtService: JwtService,
@@ -19,6 +20,7 @@ export class AuthGuard implements CanActivate {
   }
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
     const request = context.switchToHttp().getRequest();
     const token = this.extractTokenFromHeader(request);
 
@@ -27,10 +29,12 @@ export class AuthGuard implements CanActivate {
     }
 
     try {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
       const payload = await this.jwtService.verifyAsync(token, {
         secret: this.jwtSecret,
       });
 
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
       request['user'] = payload;
     } catch {
       throw new UnauthorizedException();
