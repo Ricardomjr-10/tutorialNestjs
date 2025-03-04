3f0a24b944a489910ce8ba8640162f2c9afb474c
Ricardo-10 <ricardomjr2@gmail.com>
2025-03-03 21:10:32 -0300

    validacao de dto

--- src/task/task.dto.ts
+++ src/task/task.dto.ts
@@ -1,8 +1,39 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+import {
+  IsDateString,
+  IsEnum,
+  IsOptional,
+  IsString,
+  IsUUID,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
+export enum TaskStatusEnum {
+  TO_DO = 'TO_DO',
+  IN_PROGESS = 'IN_PROGRESS',
+  DONE = 'DONE',
+}
+
 export class TaskDto {
+  @IsUUID()
   id: string;
+
+  @IsString()
+  @MinLength(3)
+  @MaxLength(256)
   title: string;
+
+  @IsString()
+  @MinLength(5)
+  @MaxLength(512)
   description: string;
+
+  @IsEnum(TaskStatusEnum)
+  @IsOptional()
   status: string;
+
+  @IsDateString()
   expirationDate: Date;
 }
 
