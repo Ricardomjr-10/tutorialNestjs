c1d29ed360627477c065d53a2dacbab506e25aee
Ricardo-10 <ricardomjr2@gmail.com>
2025-03-07 23:12:03 -0300

    bug migrations

--- src/db/migrations/1741397648329-task-table.ts
+++ src/db/migrations/1741397648329-task-table.ts
@@ -2,13 +2,13 @@ import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class TaskTable1741397648329 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`CREATE EXTENSION IF NOT EXIST "uuid-ossp";`);
+    await queryRunner.query(`CREATE EXTENSION IF NOT EXISTS "uuid-ossp";`);
     await queryRunner.query(`
             CREATE TABLE task (
-            id uuid NOT NULL DEFAULT uuid_generate_v4,
+            id uuid NOT NULL DEFAULT uuid_generate_v4(),
             title varchar(256) NOT NULL,
             description varchar(512) NULL,
-            status varchar(50) NOT NULL DEFAULT 'TO DO'.
+            status varchar(50) NOT NULL DEFAULT 'TO_DO',
             expiration_date timestamptz NOT NULL,
             CONSTRAINT task_pk PRIMARY KEY (id)
             );
