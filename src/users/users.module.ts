d1e044b31b803c14a461221b8f0fa0c2984c85b8
Ricardo-10 <ricardomjr2@gmail.com>
2025-03-08 21:38:07 -0300

    alterando para postgres

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   controllers: [UsersController],
+  imports: [TypeOrmModule],
   exports: [UsersService],
   providers: [UsersService],
 })
